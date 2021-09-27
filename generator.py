db2c34905721a169dae20d982fb37251adf6ddd9
calinanca <calinanca99@gmail.com>
2021-09-27 19:30:52 +0200

    deleted some extra code

--- generator.py
+++ generator.py
@@ -14,28 +14,21 @@ def generator():
     upper_case = "ABCDEFGHIJKLMNOQPRSTUVWXYZ"
 
     char = ""
-    pwd = ""
+    password = ""
 
     includeSymbols, includeDigits, includeLower, includeUpper = set_preferences()
 
     if includeSymbols:
         char += symbols
-
     if includeDigits:
         char += digits
-
     if includeLower:
         char += lower_case
-
     if includeUpper:
         char += upper_case
 
-    if not includeSymbols and not includeDigits and not includeLower and not includeUpper:
-        print("The password is empty!")
-        return
-
     for i in range(0, length):
         choice = random.randint(0, len(char)-1)
-        pwd += char[choice]
+        password += char[choice]
 
-    return pwd
+    return password
